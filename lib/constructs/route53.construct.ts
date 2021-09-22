1f7b304edb31319a44166dfd4498512e413b5b4e
Michael Wraa Laustsen <mwl@asseco.dk>
2021-09-22 15:58:18 +0200

    DDT-270 fix certificate and use props

--- lib/constructs/route53.construct.ts
+++ lib/constructs/route53.construct.ts
@@ -2,19 +2,19 @@ import { Construct } from '@aws-cdk/core'
 import * as route53 from '@aws-cdk/aws-route53'
 import * as acm from '@aws-cdk/aws-certificatemanager'
 
-export interface DCSARoute53Props {}
+export interface DCSARoute53Props { hostedZoneId: string}
 
 export class DCSARoute53 extends Construct {
   hostedZone: route53.IHostedZone
   hostedZoneCertificate: acm.ICertificate
 
-  constructor (scope: Construct, id: string, props: DCSARoute53Props = {}) {
+  constructor (scope: Construct, id: string, props: DCSARoute53Props = { "hostedZoneId": ""}) {
     super(scope, id)
       this.hostedZone = route53.HostedZone.fromHostedZoneAttributes(
       this,
       'dcsaHostedZone',
       {
-        hostedZoneId: "Z0356896WKOUB3ZK0XRN",
+        hostedZoneId: props.hostedZoneId,
         zoneName: "hamburg.dev.dcsa.org"
       }
     )
@@ -23,8 +23,8 @@ export class DCSARoute53 extends Construct {
       this,
       'dcsaCertificate',
       {
-        domainName: "terminal." + process.env.BASEURL,
-          subjectAlternativeNames: ["carrier." + process.env.BASEURL],
+        domainName: "dcsa." + process.env.BASEURL,
+          subjectAlternativeNames: ["evergreen-marine." + process.env.BASEURL, "hamburg-port-authority." + process.env.BASEURL, "hapag-lloyd." + process.env.BASEURL, "hvcc-hamburg." + process.env.BASEURL, "cma-cgm." + process.env.BASEURL],
         validation: acm.CertificateValidation.fromDns(this.hostedZone)
       }
     )
