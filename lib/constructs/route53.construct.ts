0ac7012fbbef9d9cda4477db56e721a2f4899a24
Michael Wraa Laustsen <mwl@asseco.dk>
2021-09-23 10:19:04 +0200

    DDT-270 Parcipants and baseurl as props
    Move values for parcipants and baseUlr to props that is instasiated from
    ENV variables set in github actions

--- lib/constructs/route53.construct.ts
+++ lib/constructs/route53.construct.ts
@@ -2,20 +2,31 @@ import { Construct } from '@aws-cdk/core'
 import * as route53 from '@aws-cdk/aws-route53'
 import * as acm from '@aws-cdk/aws-certificatemanager'
 
-export interface DCSARoute53Props { hostedZoneId: string}
+export interface DCSARoute53Props { hostedZoneId: string, baseUrl: string, participants: string[]}
 
 export class DCSARoute53 extends Construct {
   hostedZone: route53.IHostedZone
   hostedZoneCertificate: acm.ICertificate
 
-  constructor (scope: Construct, id: string, props: DCSARoute53Props = { "hostedZoneId": ""}) {
+
+  constructor (scope: Construct, id: string, props: DCSARoute53Props = { "hostedZoneId": "", baseUrl: "", "participants": []}) {
     super(scope, id)
-      this.hostedZone = route53.HostedZone.fromHostedZoneAttributes(
+    let subjectAlternativeNamesString = '';
+
+    props.participants.forEach( ((participant, i, arr) => {
+        if (i === 0) return;
+        subjectAlternativeNamesString += participant + "." + props.baseUrl;
+        if (i < arr.length -1) {
+            subjectAlternativeNamesString+= ","
+        }
+    }));
+
+    this.hostedZone = route53.HostedZone.fromHostedZoneAttributes(
       this,
       'dcsaHostedZone',
       {
         hostedZoneId: props.hostedZoneId,
-        zoneName: "hamburg.dev.dcsa.org"
+        zoneName: props.baseUrl
       }
     )
 
@@ -23,8 +34,8 @@ export class DCSARoute53 extends Construct {
       this,
       'dcsaCertificate',
       {
-        domainName: "dcsa." + process.env.BASEURL,
-          subjectAlternativeNames: ["evergreen-marine." + process.env.BASEURL, "hamburg-port-authority." + process.env.BASEURL, "hapag-lloyd." + process.env.BASEURL, "hvcc-hamburg." + process.env.BASEURL, "cma-cgm." + process.env.BASEURL],
+        domainName: props.participants[0] + props.baseUrl,
+          subjectAlternativeNames: [subjectAlternativeNamesString],
         validation: acm.CertificateValidation.fromDns(this.hostedZone)
       }
     )
