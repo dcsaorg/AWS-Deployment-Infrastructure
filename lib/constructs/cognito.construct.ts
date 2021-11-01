481db221e1f985b79f58268fa9cc47ebdcd45fc6
Martin Johannesen <martin@atlanticapps.dk>
2021-11-01 11:32:40 +0100

    Update cognito.construct.ts

--- lib/constructs/cognito.construct.ts
+++ lib/constructs/cognito.construct.ts
@@ -1,6 +1,6 @@
 import {Construct,Duration,RemovalPolicy,CfnOutput} from '@aws-cdk/core'
 import * as cognito from '@aws-cdk/aws-cognito'
-import * as rds from '@aws-cdk/aws-rds'
+import { OAuthScope } from "@aws-cdk/aws-cognito";
 
 export interface CognitoConstructProps {
     placeholder: string
@@ -11,9 +11,36 @@ export class CognitoConstruct extends Construct {
     constructor(scope: Construct, id: string, props: CognitoConstructProps) {
         super(scope, id)
 
-        new cognito.UserPool(this, 'up', {
+        const pool=new cognito.UserPool(this, 'up', {
             userPoolName: 'up',
         });
+
+        pool.addDomain('dg', {
+            cognitoDomain: {
+                domainPrefix: 'weneedsomerandomhere',
+            },
+        });
+
+        pool.addResourceServer('upre',{
+            identifier: "https://resource-server/",
+            scopes: [
+                {
+                    scopeDescription: "todo1description",
+                    scopeName: "todo1name",
+                },
+            ],
+            userPoolResourceServerName:"ourresource"
+        })
+
+        const client = pool.addClient('client1', {
+            generateSecret:true,
+            oAuth: {
+                flows: {
+                    clientCredentials: true,
+                },
+                scopes: [OAuthScope.custom("https://resource-server//todo1name")],
+            }});
+        const clientId = client.userPoolClientId;
 
     }
 }
