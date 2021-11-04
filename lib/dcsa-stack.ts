576d31867d01e61eb37067fa1988f1aef7076695
Martin Johannesen <martin@atlanticapps.dk>
2021-11-04 23:55:26 +0100

    adding db parameters

--- lib/dcsa-stack.ts
+++ lib/dcsa-stack.ts
@@ -5,7 +5,7 @@ import { DBConstruct } from "./constructs/db.construct";
 import {CognitoConstruct} from "./constructs/cognito.construct";
 
 
-export interface DCSAStackProps extends cdk.StackProps { hostedZoneId: string, baseUrl: string, participants: string, cognitoUserPoolId: string, helmVersion: string, springMailUsername: string,experimental:string}
+export interface DCSAStackProps extends cdk.StackProps { hostedZoneId: string, baseUrl: string, participants: string, cognitoUserPoolId: string, helmVersion: string, springMailUsername: string,experimental:string,dbpassword:string}
 
 export class DCSAStack extends cdk.Stack {
   constructor (scope: cdk.Construct, id: string, props: DCSAStackProps) {
@@ -21,13 +21,13 @@ export class DCSAStack extends cdk.Stack {
 
     const { tokenUrl,dcsaClientSecret,dcsaClientId,uiClientId,cognitoUserPoolId } =  new CognitoConstruct(this, "cg", {participants: props.participants});
 
-    if(experimental==true) {
-    new DBConstruct(this, "db", {"placeholder": "placeholdertext"});
 
-    }
+   const {dbHostname,dbPort}= new DBConstruct(this, "db", {"placeholder": "placeholdertext"});
+
+
 
       new DCSAEKSCluster(this, 'EKSCluster', {
-        hostedZoneCertificate, "cognitoUserPoolId": cognitoUserPoolId, "helmVersion": props.helmVersion, "participants": props.participants, "springMailUsername": props.springMailUsername,experimental: experimental,cognitoUIClientId: uiClientId,cognitoDCSAClientId:dcsaClientId,cognitoDCSAClientSecret:dcsaClientSecret,cognitoTokenUrl:tokenUrl
+        hostedZoneCertificate, "cognitoUserPoolId": cognitoUserPoolId, "helmVersion": props.helmVersion, "participants": props.participants, "springMailUsername": props.springMailUsername,experimental: experimental,cognitoUIClientId: uiClientId,cognitoDCSAClientId:dcsaClientId,cognitoDCSAClientSecret:dcsaClientSecret,cognitoTokenUrl:tokenUrl,dbPort:dbPort,dbHost:dbHostname,dbPassword: props.dbpassword
       })
 
 
