9f9225298bb49ff72c6bad201b59d8600c53c16b
Michael Wraa Laustsen <mwl@asseco.dk>
2021-12-08 08:39:29 +0100

    Update cognito.construct.ts

--- lib/constructs/cognito.construct.ts
+++ lib/constructs/cognito.construct.ts
@@ -24,7 +24,7 @@ export class CognitoConstruct extends Construct {
     constructor(scope: Construct, id: string, props: CognitoConstructProps) {
         super(scope, id);
 
-        //if(props.cognitoUserPoolId.length>0) {
+        if(props.cognitoUserPoolId.length>0) {
             this.cognitoUserPoolId= props.cognitoUserPoolId;
             this.dcsaClientId= props.dcsaClientId;
             this.dcsaClientSecret= props.dcsaClientSecret;
@@ -32,7 +32,7 @@ export class CognitoConstruct extends Construct {
             this.uiClientId= props.uiClientId;
             //this.uiClientSecret= props.uiClientSecret;
             return
-        //}
+        }
 
 
         const pool=new cognito.UserPool(this, 'up', {
