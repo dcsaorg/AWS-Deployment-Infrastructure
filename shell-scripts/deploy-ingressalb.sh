165a57d49ec96943eb1270c2bcc9b868da26e41b
Martin Johannesen <martin@atlanticapps.dk>
2022-01-18 13:17:01 +0000

    Update deploy-ingressalb.sh

--- shell-scripts/deploy-ingressalb.sh
+++ shell-scripts/deploy-ingressalb.sh
@@ -9,20 +9,24 @@ helm repo add dcsa https://dcsaorg.github.io/Kubernetes-Packaging/
 certificateArn=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey|test("hostedZoneCertificateArn")) | .OutputValue' stack-out.json)
 participantNames=$(echo "$PARTICIPANTS" | jq -r '.[].name')
 
-echo "Creating values values.yml for $participantNames"
+echo "Creating values values.yml"
 
-    participantTrimmed=$(echo $participant | tr -d '-')
-    partycode=$(echo "$PARTICIPANTS" | jq -r ".[] | select(.name|test(\"$participant\")) | .partycode")
-    publisherroles=$(echo "$PARTICIPANTS" | jq -r ".[] | select(.name|test(\"$participant\")) | .publisherroles")
-    email=$(echo "$PARTICIPANTS" | jq -r ".[] | select(.name|test(\"$participant\")) | .email")
-
-    cat <<EOF >> values.yml
+cat <<EOF >> values-1.yml
 certificateArn: "$certificateArn"
 
 env:
     baseurl: "$BASEURL"
-    participants: "$participant"
+    participants:
 EOF
+
+for p in $participantNames; do
+    cat <<EOF >> values-2.yml
+      - "$p"
+EOF
+done
+
+cat values-*.yml > values.yml
+rm values-*.yml
 cat values.yml
 echo "Deploying ingress helm for $participantNames"
 helm install ingressdcsa dcsa/dcsaingresscluster --values values.yml
