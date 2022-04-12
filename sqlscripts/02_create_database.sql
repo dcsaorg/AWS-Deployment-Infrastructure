b1a05c4f9942a26f979c793209f021ac42881ec3
Michael Wraa Laustsen <mwl@asseco.dk>
2022-04-12 09:58:12 +0200

    ADS-152

--- sqlscripts/02_create_database.sql
+++ sqlscripts/02_create_database.sql
@@ -5,12 +5,12 @@
 -- Cannot be done in a transaction
 DROP DATABASE IF EXISTS :dcsadbname;
 CREATE DATABASE :"dcsadbname"; --OWNER dcsa_db_owner;
-\connect :dcsadbname
+\connect :"dcsadbname"
 
 BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Used to generate UUIDs
 CREATE SCHEMA IF NOT EXISTS dcsa_im_v3_0;
---GRANT ALL PRIVILEGES ON DATABASE :dcsadbname TO dcsa_db_owner;
+--GRANT ALL PRIVILEGES ON DATABASE :"dcsadbname" TO dcsa_db_owner;
 --GRANT ALL PRIVILEGES ON SCHEMA dcsa_im_v3_0 TO dcsa_db_owner;
 --ALTER DEFAULT PRIVILEGES IN SCHEMA dcsa_im_v3_0 GRANT ALL ON TABLES TO dcsa_db_owner;
 COMMIT;
