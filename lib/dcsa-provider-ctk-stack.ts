565574c98e183c62e20b18fdb1e69c3b0a49b9b1
Martin Johannesen <martin@atlanticapps.dk>
2022-08-29 16:25:23 +0200

    Update dcsa-provider-ctk-stack.ts

--- lib/dcsa-provider-ctk-stack.ts
+++ lib/dcsa-provider-ctk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import { DCSARoute53 } from './constructs/route53.construct'
-import { DCSAEKSCluster } from './constructs/provider-ctk-cluster.construct'
-import { DBConstruct } from "./constructs/db.construct";
+import { DCSAEKSCluster } from './constructs/eks-cluster.construct'
+//import { DBConstruct } from "./constructs/db.construct";
 import { CfnOutput } from '@aws-cdk/core';
 
 
