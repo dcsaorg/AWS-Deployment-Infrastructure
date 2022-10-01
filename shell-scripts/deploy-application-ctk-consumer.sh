3835cd59ab75aa3841d81df986002f1ae959762e
Martin Johannesen <martin@atlanticapps.dk>
2022-10-01 21:40:42 +0200

    more on ctk

---
+++ shell-scripts/deploy-application-ctk-consumer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+aws cloudformation describe-stacks --stack-name st > st-stack-out.json
+aws cloudformation describe-stacks --stack-name cognito > cognito-stack-out.json
+aws cloudformation describe-stacks --stack-name db > db-stack-out.json
+jq -r '.Stacks[0].Outputs[] | select(.OutputKey|test("ConfigCommand")) | .OutputValue' st-stack-out.json > ./kube.sh
+. ./kube.sh
+
+helm repo add postgresql https://charts.bitnami.com/bitnami
+helm install postgresql postgresql/postgresql --values ctk/charts/valuesps.yml
+
+helm list
