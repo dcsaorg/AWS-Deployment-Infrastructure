024caf51ba61a9337fc9319fb86f092ba8ca8de1
Martin Johannesen <martin@atlanticapps.dk>
2022-10-17 16:15:31 +0200

    more on cognito in gateway

--- lib/apigateway-stack.ts
+++ lib/apigateway-stack.ts
@@ -1,4 +1,4 @@
-import {Construct} from '@aws-cdk/core'
+import {Construct, RemovalPolicy, Stack} from '@aws-cdk/core'
 import * as cdk from "@aws-cdk/core";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as apigateway from "@aws-cdk/aws-apigateway";
@@ -9,18 +9,36 @@ import {
 import * as acm from '@aws-cdk/aws-certificatemanager'
 import * as route53 from '@aws-cdk/aws-route53'
 
-export interface DCSAAPIGatewayProps  {
+export interface DCSAAPIGatewayProps extends cdk.StackProps {
     baseUrl: string,
-    userpoolId: string
     hostedZoneId:string
 }
 
 
-export class DCSAAPIGateway extends Construct {
+export class DCSAAPIGateway extends cdk.Stack {
     constructor(scope: Construct, id: string, props: DCSAAPIGatewayProps) {
         super(scope, id)
 
+        const pool=new cognito.UserPool(this, 'up', {
+            selfSignUpEnabled: false,
+            userPoolName: 'up',
+            customAttributes: {
+                groups: new cognito.StringAttribute({ minLen: 1, maxLen: 255 }),
+            },
+        });
+        pool.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+        const urlid=makeid(10);
+
+        pool.addDomain('dg', {
+            cognitoDomain: {
+                domainPrefix: urlid,
+            },
+        });
+
+
         //Certificate
+
 
         var domainname = "dev1." + props.baseUrl;
 
@@ -40,13 +58,6 @@ export class DCSAAPIGateway extends Construct {
             subjectAlternativeNames: [],
             region: cdk.Aws.REGION,
         });
-
-        /*const existingUserPool = cognito.UserPool.fromUserPoolId(
-            this,
-            "upool",
-            props.userpoolId
-        );*/
-
 
 
         /*const authorizer = new CognitoUserPoolsAuthorizer(
@@ -84,4 +95,15 @@ export class DCSAAPIGateway extends Construct {
 
 
     }
+}
+
+function makeid(length:number) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
 }
