b5429e2b036443d25ba92d49d9702a772e636081
Martin Johannesen <martin@atlanticapps.dk>
2022-10-19 17:41:04 +0200

    Update apigateway-stack.ts

--- lib/apigateway-stack.ts
+++ lib/apigateway-stack.ts
@@ -97,11 +97,11 @@ export class DCSAAPIGateway extends cdk.Stack {
             restApiName: "Secured APIGateway",
             description: "This API is the secure apis",
 
-            domainName: {
+            /*domainName: {
                 domainName: domainname,
                 certificate: certificate,
                 endpointType: EndpointType.REGIONAL,
-            },
+            },*/
         });
 
         new route53.ARecord(this, "apiDNS", {
