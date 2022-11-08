d3225a4663521c3dfbfac31771001f942b257deb
Martin Johannesen <martin@linesystems.io>
2022-11-08 14:17:52 +0100

    Update route53.construct.ts

--- lib/constructs/route53.construct.ts
+++ lib/constructs/route53.construct.ts
@@ -43,7 +43,7 @@ export class DCSARoute53 extends Construct {
       this,
       'dcsaCertificate',
       {
-        domainName: 'dcsa.'+props.baseUrl,
+        domainName: firstParticipant+'.'+props.baseUrl,
         subjectAlternativeNames: subjectAlternativeNames,
         validation: acm.CertificateValidation.fromDns(this.hostedZone)
       }
