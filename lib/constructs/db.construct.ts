aeff9941d83849c3a7aef75fbed2141432715f8e
Martin Johannesen <martin@linesystems.io>
2023-03-08 15:33:58 +0100

    more on packages

--- lib/constructs/db.construct.ts
+++ lib/constructs/db.construct.ts
@@ -1,4 +1,4 @@
-import {Duration,RemovalPolicy,CfnOutput} from 'aws-cdk-lib/core'
+import {Duration,RemovalPolicy,CfnOutput} from 'aws-cdk-lib'
 import {Construct} from 'constructs'
 import * as cdk from 'aws-cdk-lib/core'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
