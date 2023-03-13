6f15a7c037aba8351c6a9517ff26854bdbb77065
Martin Johannesen <martin@linesystems.io>
2023-03-13 09:00:14 +0100

    Update deploy-application-ap-booking.sh

--- shell-scripts/deploy-application-ap-booking.sh
+++ shell-scripts/deploy-application-ap-booking.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws cloudformation describe-stacks --stack-name st > st-stack-out.json
+aws cloudformation describe-stacks --stack-name stnlb > st-stack-out.json
 jq -r '.Stacks[0].Outputs[] | select(.OutputKey|test("ConfigCommand")) | .OutputValue' st-stack-out.json > ./kube.sh
 . ./kube.sh
 
