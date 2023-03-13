3ed7cf15925d4e7a56eee14c8e61eb7f31bc1896
Martin Johannesen <martin@linesystems.io>
2023-03-13 08:57:23 +0100

    more on ap-booking

---
+++ shell-scripts/deploy-application-ap-booking.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+aws cloudformation describe-stacks --stack-name st > st-stack-out.json
+jq -r '.Stacks[0].Outputs[] | select(.OutputKey|test("ConfigCommand")) | .OutputValue' st-stack-out.json > ./kube.sh
+. ./kube.sh
+
+certificateArn=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey|test("hostedZoneCertificateArn")) | .OutputValue' st-stack-out.json)
+
+
+    cat <<EOF >> values.yml
+certificateArn: "$certificateArn"
+
+env:
+    baseurl: "$BASEURL"
+    participant: "dcsa"
+
+
+EOF
+
+    echo "Deploying helm for dcsa"
+    helm upgrade --install "dcsa" ./charts/ap-booking --values values.yml
+
+
+
+
+
+helm list
